b62699cd11c0a7c1d8dd4f6e25c13533f11756aa
ALLJISAN <873839260@qq.com>
2017-12-13 12:36:53 +0900

    Add some decription

--- Cookie-Problem.py
+++ Cookie-Problem.py
@@ -3,6 +3,7 @@
 from thinkbayes import Pmf
 
 class Bowl():
+    #Initial bow1&bowl2 sugars
     def __init__(self,bowl):
         total=100
         if bowl==1:
@@ -11,20 +12,24 @@ class Bowl():
         if bowl==2:
             self.vanilla=50
             self.chocolate=50
+    #get the probability of two kinds of sugar
     def get_bowl_prob(self):
         self.vanilla_prob=float(self.vanilla)/(self.vanilla+self.chocolate)
         self.chocolate_prob=float(self.chocolate)/(self.vanilla+self.chocolate)
+    #Every running(take out one vanilla from the bowl)
     def MM_change(self):
         self.vanilla-=1 
             
             
 class Cookie(Pmf,Bowl):
+    #Use PMF in mouduel 'thinkbayes' to initialize the proability
+    #in https://github.com/AllenDowney/ThinkBayes/
     def __init__(self,hypos):
         Pmf.__init__(self)
         for hypo_k,hypo_v in hypos.items():
             self.Set(hypo_k,1)
         self.Normalize()
-        
+    #Update and get the probability we want
     def Update(self,data,hypos):
         for hypo in self.Values():
             like=self.LikeHood(data,hypo,hypos)
@@ -34,6 +39,7 @@ class Cookie(Pmf,Bowl):
             v.MM_change()
             
         self.Normalize()
+    #Get the likehood
     def LikeHood(self,data,hypo,hypos):
         if data=='vanilla':
             mix=hypos[hypo]
@@ -43,13 +49,16 @@ class Cookie(Pmf,Bowl):
             like=mix.chocolate_prob
         return like
     
-    
+#Start running
 Bow1=Bowl(1)
 Bow1.get_bowl_prob()
 Bow2=Bowl(2)
 Bow2.get_bowl_prob()
+#A dict with Class Bowl contain :vanilla,chocolate，and their prob
 hypos={'Bowl 1':Bow1,'Bowl 2':Bow2}
 pmf=Cookie(hypos)
+#take 3 sugars and calculate the probability that if you take out a
+#vanilla flavor ,what is the probability of where it from,bowl1,or bowl2
 for i in range(3):
     print "After {} Times".format(i)
     pmf.Update('vanilla',hypos)
